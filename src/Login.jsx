1f5d638099409087eb0ee3e2297e40c164bb9811
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2021-05-06 14:09:09 +0530

    SpotifyWebApi added

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,14 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './login.css'
-
 import { authURL, sendbackURL } from './spotify.js'
-
+import SpotifyWebApi from 'spotify-web-api-js'
 
 function Login() {
 
+    const [token,setToken] = useState(null)
+    const spotify = new SpotifyWebApi()
+
     useEffect(() => {
-        console.log(sendbackURL())
+        const _token = sendbackURL()
+        console.log(_token)
         window.location.hash = ""
+
+        if (_token) {
+            setToken(_token)
+            spotify.setAccessToken(_token)
+
+            spotify.getMe().then((user) => {
+                console.log(user);
+            })
+
+        }
+
     }, [])
 
     return (
