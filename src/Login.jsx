8c839512c62ac0e369708f4e3e89084000be994e
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2021-05-06 10:15:00 +0530

    extract token from URI

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './login.css'
 
-import {authURL} from './spotify.js'
+import { authURL, sendbackURL } from './spotify.js'
+
 
 function Login() {
+
+    useEffect(() => {
+        console.log(sendbackURL())
+        window.location.hash = ""
+    }, [])
+
     return (
         <div className="login">
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify-logo"></img>
             <a href={authURL}>Login with spotify</a>
-            
+
         </div>
     )
 }
--- src/spotify.js
+++ src/spotify.js
@@ -10,6 +10,12 @@ const scopes = [
     "user-modify-playback-state",
 ]
 
+export const sendbackURL = () => {
+    let url_array = window.location.hash.substring(1).split("&")
+    let tok = url_array[0].split("=")
+    return tok[1]
+} 
+ 
 export const authURL = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
 
 // auth_endpoint + "?" + clientId + "&" + redirectUri  + "&" + "scope=" + scopes.join("%20") +  "&" + "response_type=token "
